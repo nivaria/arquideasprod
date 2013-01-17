898dc206973dfdcb79bbb2ce3a395c3a252ce9b0
Kaloyan Petrov <kaloyan@designolog.com>
2013-01-18 00:37:59 +0200

    Contest detail page styles

--- templates/node/node-contest-open.tpl.php
+++ templates/node/node-contest-open.tpl.php
@@ -4,131 +4,116 @@
 
 <div id="node-<?php print $node->nid; ?>" class="node node-contest-open <?php print $node_classes; ?>">
   <div class="inner">
-    <!-- IMAGE --> 
-    <?php 
-        if(isset($field_detail_image_contest[0]['filepath']) && !$is_edit){
-            $preset = variable_get('nivaria_contests_base_preset_contest_closed', 'Featured');
-            print theme_imagecache($preset, $field_detail_image_contest[0]['filepath'], $title);
-        } 
-    ?>
-    <!-- END IMAGE -->
+    <div id="contest-banner">
+      <!-- IMAGE -->
+      <?php if(isset($field_detail_image_contest[0]['filepath']) && !$is_edit){
+              $preset = variable_get('nivaria_contests_base_preset_contest_closed', 'Featured');
+              print theme_imagecache($preset, $field_detail_image_contest[0]['filepath'], $title);
+          }
+      ?>
+      <!-- END IMAGE -->
 
-    <!-- TITLE -->
-    <?php if ($page == 0): ?>
-    <h2 class="title"><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-    <?php endif; ?>
-    <?php if ($page == 1): ?>
-    <h1 class="title"><?php print $title; ?></h1>
-    <?php endif; ?>
-    <!-- END TITLE -->
-    
-    <!-- Edit Button -->
-    <?php if(!$is_edit): ?>  
-        <?php if(node_access('update', $node)){
-            print l('<span>'.t('Edit').'</span>','node/'.$node->nid.'/edit',array(
-                'attributes' => array(
-                    'title' => t('Edit'),
-                    'class' => 'edit-content-link',
-                    'style' => 'top: 16px; right: 16px;',
-                ),
-                'html' => TRUE,
-            ));
-        } ?> 
-    <?php endif; ?>
-    <!-- End Edit Button -->
-    
-    <!-- TWITTER HASH TAG -->
-    <?php
-        if(isset($field_contest_twitter_hashtag[0]['value']) && !$is_edit):
-    ?>        
-    <div class="contest-twitter-hashtag">
-        <?php if(_twitterfield_is_hashtag($field_contest_twitter_hashtag[0]['value'])){
-            $url_part = 'search?q=' . rawurlencode($field_contest_twitter_hashtag[0]['value']);
-            print l($field_contest_twitter_hashtag[0]['value'], 'http://twitter.com/' . $url_part, array('attributes' => array('class' => 'twitterfield_twitter_link')));
-        } else {
-            print $field_contest_twitter_hashtag[0]['value']; 
-        }    
-        ?>
-    </div>        
-    <?php        
-        endif;
-    ?>
-    <!-- END TWITTER HASH TAG -->
-    
-    <!-- TAG LINE -->
-    <?php
-        if(isset($field_contest_tagline[0]['value']) && !$is_edit):
-    ?>        
-    <div class="contest-tagline">
-        <?php print $field_contest_tagline[0]['value']; ?>
-    </div>    
-    <?php        
-        endif;
-    ?>
-    <!-- END TAG LINE -->
+      <div class="contest-titles">
+        <!-- TITLE -->
+        <?php if ($page == 0): ?>
+        <h2 class="title"><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+        <?php endif; ?>
+        <?php if ($page == 1): ?>
+        <h1 class="title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <!-- END TITLE -->
 
-    <!-- CONTEST STATE -->
-    <?php
-        if(isset($field_contest_state[0]['value']) && !$is_edit):
-    ?>        
-    <div class="contest-state">
-        <?php print getContestStateName(intval($field_contest_state[0]['value'])); ?>
-    </div>        
-    <?php        
-        endif;
-    ?>
-    <!-- END CONTEST STATE -->
-    
-    <!-- INSCRIPTION LIMIT DATE -->
-    <?php
-        if(!$is_edit){
-            print showOpenContestDate($node);
-        }
-    ?>
-    <!-- END INSCRIPTION LIMIT DATE -->
-    
-    <div class="clearfix">&nbsp;</div>
-    
-    <!-- INSCRIPTION, PAYMENT OR PRESENTATION LINK -->
-    <?php
-        if(!$is_edit){
-            print '<div class="open-contest-button">'.openContestButton($node,TRUE).'</div>'; 
-        }    
-    ?>
-    <!-- END INSCRIPTION, PAYMENT OR PRESENTATION LINK -->
-    
-    <!-- SHARE SOCIAL BLOCK -->
-    <?php
-        $block = module_invoke('arquideas_generic', 'block', 'view', '13');
-        print $block['content'];
-    ?>    
-    <!-- END SHARE SOCIAL BLOCK -->
-    
-    
-    <!-- FOLLOW OR LEAVE BUTTON -->
-    <?php if(!$is_edit) print showContestSuscribeLink($node); ?>
-    <!-- END FOLLOW OR LEAVE BUTTON -->
-    
-    <!-- FOLLOWERS NUMBER -->
-    <?php if(!$is_edit) print showNumContestFollowers($node); ?>
-    <!-- END FOLLOWERS NUMBER -->
-    
-    <!-- COMMENTS NUMBER FROM OG_STATISTICS -->
-    <?php $num_comments = 0; 
-    if(!$is_edit && !empty($node->field_contest_related_group[0]['nid'])){
-        $stat = og_statistics_load($node->field_contest_related_group[0]['nid']);
-        $num_comments = $stat['comments_count'];
-    }
-    ?>
-    <?php if(!$is_edit): ?>
-        <div class="contest-comments">
-            <span>
-                <?php print $num_comments; ?>
-            </span>
+        <!-- TAG LINE -->
+        <?php if(isset($field_contest_tagline[0]['value']) && !$is_edit): ?>
+        <div class="contest-tagline">
+            <?php print $field_contest_tagline[0]['value']; ?>
         </div>
-    <?php endif; ?>
-    <!-- END COMMENTS NUMBER FROM OG_STATISTICS -->
-    
+        <?php endif; ?>
+        <!-- END TAG LINE -->
+      </div>
+
+      <div class="content-stats">
+        <!-- INSCRIPTION LIMIT DATE -->
+        <?php if(!$is_edit) { print showOpenContestDate($node); } ?>
+        <!-- END INSCRIPTION LIMIT DATE -->
+        <!-- TWITTER HASH TAG -->
+        <?php if(isset($field_contest_twitter_hashtag[0]['value']) && !$is_edit): ?>
+        <div class="contest-twitter-hashtag">
+            <?php if(_twitterfield_is_hashtag($field_contest_twitter_hashtag[0]['value'])){
+                $url_part = 'search?q=' . rawurlencode($field_contest_twitter_hashtag[0]['value']);
+                print l($field_contest_twitter_hashtag[0]['value'], 'http://twitter.com/' . $url_part, array('attributes' => array('class' => 'twitterfield_twitter_link')));
+            } else {
+                print $field_contest_twitter_hashtag[0]['value'];
+            }
+            ?>
+        </div>
+        <?php endif; ?>
+        <!-- END TWITTER HASH TAG -->
+      </div>
+
+      <div class="contest-state-wrapper">
+        <!-- CONTEST STATE -->
+        <?php if(isset($field_contest_state[0]['value']) && !$is_edit): ?>
+        <div class="contest-state">
+            <?php print getContestStateName(intval($field_contest_state[0]['value'])); ?>
+        </div>
+        <?php endif; ?>
+        <!-- END CONTEST STATE -->
+
+        <!-- INSCRIPTION, PAYMENT OR PRESENTATION LINK -->
+        <?php if(!$is_edit){ print '<div class="open-contest-button">'.openContestButton($node,TRUE).'</div>'; } ?>
+        <!-- END INSCRIPTION, PAYMENT OR PRESENTATION LINK -->
+      </div>
+
+      <div class="clearfix">&nbsp;</div>
+
+      <!-- SHARE SOCIAL BLOCK -->
+      <?php
+          $block = module_invoke('arquideas_generic', 'block', 'view', '13');
+          print $block['content'];
+      ?>
+      <!-- END SHARE SOCIAL BLOCK -->
+
+      <!-- Edit Button -->
+      <?php if(!$is_edit): ?>
+          <?php if(node_access('update', $node)){
+              print l('<span>'.t('Edit').'</span>','node/'.$node->nid.'/edit',array(
+                  'attributes' => array(
+                      'title' => t('Edit'),
+                      'class' => 'edit-content-link',
+                      'style' => 'top: 16px; right: 16px;',
+                  ),
+                  'html' => TRUE,
+              ));
+          } ?>
+      <?php endif; ?>
+      <!-- End Edit Button -->
+      <div class="social-corner">
+        <!-- FOLLOWERS NUMBER -->
+        <?php if(!$is_edit) print showNumContestFollowers($node); ?>
+        <!-- END FOLLOWERS NUMBER -->
+
+        <!-- COMMENTS NUMBER FROM OG_STATISTICS -->
+        <?php $num_comments = 0;
+        if(!$is_edit && !empty($node->field_contest_related_group[0]['nid'])){
+            $stat = og_statistics_load($node->field_contest_related_group[0]['nid']);
+            $num_comments = $stat['comments_count'];
+        }
+        ?>
+        <?php if(!$is_edit): ?>
+            <div class="contest-comments">
+                <span>
+                    <?php print $num_comments; ?>
+                </span>
+            </div>
+        <?php endif; ?>
+        <!-- END COMMENTS NUMBER FROM OG_STATISTICS -->
+        <!-- FOLLOW OR LEAVE BUTTON -->
+        <?php if(!$is_edit) print showContestSuscribeLink($node); ?>
+        <!-- END FOLLOW OR LEAVE BUTTON -->
+      </div>
+    </div>
+
     <?php if ($node_top && !$teaser): ?>
     <div id="node-top" class="node-top row nested">
       <div id="node-top-inner" class="node-top-inner inner">
@@ -148,14 +133,14 @@
       <?php print $terms; ?>
     </div>
     <?php endif;?>
-    
+
     <div class="content clearfix<?php print ($node_right && !$teaser?' node-right':''); ?>">
-        <div class="node-content-main">  
+        <div class="node-content-main">
             <?php
                 /* QUICK TABS */
                 if(!$is_edit && isset($contest_quicktab)){
                     print $contest_quicktab;
-                }    
+                }
                 /* END QUICK TABS */
                 $content = str_replace($body, '', $content);
                 print $content;
@@ -169,7 +154,7 @@
         </div><!-- /node-right -->
         <?php endif; ?>
     </div>
-    
+
     <!-- Translate link -->
     <?php if(isset($node->content['nivaria_translate_content_link']) && !empty($node->content['nivaria_translate_content_link']['#value'])): ?>
     <div class="node-translate-link">
@@ -177,7 +162,7 @@
     </div>
     <?php endif; ?>
     <!-- End Translate link -->
-    
+
     <!-- Mark Highlighted in Newsletter -->
     <?php if(user_access(PERM_ADMIN_CONTESTS)) : ?>
     <div class="link-highlight-arquideas-newsletter">
@@ -191,7 +176,7 @@
       <?php print $links; ?>
     </div>
     <?php endif; ?>
-    
+
   </div><!-- /inner -->
 
   <?php if ($node_bottom && !$teaser): ?>
--- templates/node/node-contest.tpl.php
+++ templates/node/node-contest.tpl.php
@@ -4,144 +4,136 @@
 
 <div id="node-<?php print $node->nid; ?>" class="node <?php print $node_classes; ?>">
   <div class="inner">
-    <!-- IMAGE --> 
-    <?php 
-        if(isset($field_detail_image_contest[0]['filepath']) && !$is_edit){
-            $preset = variable_get('nivaria_contests_base_preset_contest_closed', 'Featured');
-            print theme_imagecache($preset, $field_detail_image_contest[0]['filepath'], $title);
-        } 
-    ?>
-    <!-- END IMAGE -->
+    <div id="contest-banner">
+      <!-- IMAGE -->
+      <?php if(isset($field_detail_image_contest[0]['filepath']) && !$is_edit){
+              $preset = variable_get('nivaria_contests_base_preset_contest_closed', 'Featured');
+              print theme_imagecache($preset, $field_detail_image_contest[0]['filepath'], $title);
+          }
+      ?>
+      <!-- END IMAGE -->
 
-    <!-- TITLE -->
-    <?php if ($page == 0): ?>
-    <h2 class="title"><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-    <?php endif; ?>
-    <?php if ($page == 1): ?>
-    <h1 class="title"><?php print $title; ?></h1>
-    <?php endif; ?>
-    <!-- END TITLE -->
-    
-    <!-- Edit Button -->
-    <?php if(!$is_edit): ?>  
-        <?php if(node_access('update', $node)){
-            print l('<span>'.t('Edit').'</span>','node/'.$node->nid.'/edit',array(
-                'attributes' => array(
-                    'title' => t('Edit'),
-                    'class' => 'edit-content-link',
-                    'style' => 'top: 16px; right: 16px;',
-                ),
-                'html' => TRUE,
-            ));
-        } ?> 
-    <?php endif; ?>
-    <!-- End Edit Button -->
-    
-    <!-- TWITTER HASH TAG -->
-    <?php
-        if(isset($field_contest_twitter_hashtag[0]['value']) && !$is_edit):
-    ?>        
-    <div class="contest-twitter-hashtag">
-        <?php if(_twitterfield_is_hashtag($field_contest_twitter_hashtag[0]['value'])){
-            $url_part = 'search?q=' . rawurlencode($field_contest_twitter_hashtag[0]['value']);
-            print l($field_contest_twitter_hashtag[0]['value'], 'http://twitter.com/' . $url_part, array('attributes' => array('class' => 'twitterfield_twitter_link')));
-        } else {
-            print $field_contest_twitter_hashtag[0]['value']; 
-        }    
+      <div class="contest-titles">
+        <!-- TITLE -->
+        <?php if ($page == 0): ?>
+        <h2 class="title"><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+        <?php endif; ?>
+        <?php if ($page == 1): ?>
+        <h1 class="title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <!-- END TITLE -->
+
+        <!-- TAG LINE -->
+        <?php if(isset($field_contest_tagline[0]['value']) && !$is_edit): ?>
+        <div class="contest-tagline">
+            <?php print $field_contest_tagline[0]['value']; ?>
+        </div>
+        <?php endif; ?>
+        <!-- END TAG LINE -->
+      </div>
+
+      <div class="content-stats">
+        <!-- NUMBER OF TEAMS -->
+        <?php if(!$is_edit): ?>
+        <div class="number-teams">
+            <div class="label">
+                <?php print t('Number of teams participated'); ?>
+            </div>
+            <div class="count">
+                <?php print getTeamsCount($node); ?>
+            </div>
+        </div>
+        <?php endif; ?>
+        <!-- END NUMBER OF TEAMS -->
+        <!-- TWITTER HASH TAG -->
+        <?php if(isset($field_contest_twitter_hashtag[0]['value']) && !$is_edit): ?>
+        <div class="contest-twitter-hashtag">
+            <?php if(_twitterfield_is_hashtag($field_contest_twitter_hashtag[0]['value'])){
+                $url_part = 'search?q=' . rawurlencode($field_contest_twitter_hashtag[0]['value']);
+                print l($field_contest_twitter_hashtag[0]['value'], 'http://twitter.com/' . $url_part, array('attributes' => array('class' => 'twitterfield_twitter_link')));
+            } else {
+                print $field_contest_twitter_hashtag[0]['value'];
+            }
+            ?>
+        </div>
+        <?php endif; ?>
+        <!-- END TWITTER HASH TAG -->
+      </div>
+
+      <div class="contest-state-wrapper">
+        <!-- CONTEST STATE -->
+        <?php if(isset($field_contest_state[0]['value']) && !$is_edit): ?>
+        <div class="contest-state">
+            <?php print getContestStateName(intval($field_contest_state[0]['value'])); ?>
+        </div>
+        <?php endif; ?>
+        <!-- END CONTEST STATE -->
+
+        <!-- Public Votation Link -->
+        <?php if(!$is_edit): ?>
+            <?php global $user;
+            if($node->field_contest_state[0]['value']==ContestState::PUBLIC_CONTEST && $user->uid!=0){
+                print l('<span>'.t('Vote your favorite').'</span>','contest/'.$node->nid.'/publicvotation',array(
+                    'attributes' => array(
+                        'title' => t('Vote your favorite'),
+                        'class' => 'public-votation-link',
+                    ),
+                    'html' => TRUE,
+                ));
+            } ?>
+        <?php endif; ?>
+        <!-- End Public Votation Link -->
+      </div>
+
+      <div class="clearfix">&nbsp;</div>
+
+      <!-- SHARE SOCIAL BLOCK -->
+      <?php
+          $block = module_invoke('arquideas_generic', 'block', 'view', '13');
+          print $block['content'];
+      ?>
+      <!-- END SHARE SOCIAL BLOCK -->
+
+      <!-- Edit Button -->
+      <?php if(!$is_edit): ?>
+          <?php if(node_access('update', $node)){
+              print l('<span>'.t('Edit').'</span>','node/'.$node->nid.'/edit',array(
+                  'attributes' => array(
+                      'title' => t('Edit'),
+                      'class' => 'edit-content-link',
+                      'style' => 'top: 16px; right: 16px;',
+                  ),
+                  'html' => TRUE,
+              ));
+          } ?>
+      <?php endif; ?>
+      <!-- End Edit Button -->
+      <div class="social-corner">
+        <!-- FOLLOWERS NUMBER -->
+        <?php if(!$is_edit) print showNumContestFollowers($node); ?>
+        <!-- END FOLLOWERS NUMBER -->
+
+        <!-- COMMENTS NUMBER FROM OG_STATISTICS -->
+        <?php $num_comments = 0;
+        if(!$is_edit && !empty($node->field_contest_related_group[0]['nid'])){
+            $stat = og_statistics_load($node->field_contest_related_group[0]['nid']);
+            $num_comments = $stat['comments_count'];
+        }
         ?>
-    </div>        
-    <?php        
-        endif;
-    ?>
-    <!-- END TWITTER HASH TAG -->
-
-    <!-- TAG LINE -->
-    <?php
-        if(isset($field_contest_tagline[0]['value']) && !$is_edit):
-    ?>        
-    <div class="contest-tagline">
-        <?php print $field_contest_tagline[0]['value']; ?>
-    </div>    
-    <?php        
-        endif;
-    ?>
-    <!-- END TAG LINE -->
-    
-    <!-- CONTEST STATE -->
-    <?php
-        if(isset($field_contest_state[0]['value']) && !$is_edit):
-    ?>        
-    <div class="contest-state">
-        <?php print getContestStateName(intval($field_contest_state[0]['value'])); ?>
-    </div>        
-    <?php        
-        endif;
-    ?>
-    <!-- END CONTEST STATE -->
-    
-    <!-- NUMBER OF TEAMS -->
-    <?php
-        if(!$is_edit):
-    ?>        
-    <div class="number-teams">
-        <div class="label">
-            <?php print t('Number of teams participated'); ?>
-        </div>  
-        <div class="count">
-            <?php print getTeamsCount($node); ?>
-        </div>
+        <?php if(!$is_edit): ?>
+            <div class="contest-comments">
+                <span>
+                    <?php print $num_comments; ?>
+                </span>
+            </div>
+        <?php endif; ?>
+        <!-- END COMMENTS NUMBER FROM OG_STATISTICS -->
+        <!-- FOLLOW OR LEAVE BUTTON -->
+        <?php if(!$is_edit) print showContestSuscribeLink($node); ?>
+        <!-- END FOLLOW OR LEAVE BUTTON -->
+      </div>
     </div>
-    <?php        
-        endif;
-    ?>
-    <!-- END NUMBER OF TEAMS -->
-    
-    <!-- SHARE SOCIAL BLOCK -->
-    <?php
-        $block = module_invoke('arquideas_generic', 'block', 'view', '13');
-        print $block['content'];
-    ?>    
-    <!-- END SHARE SOCIAL BLOCK -->
-    
-    <!-- FOLLOW OR LEAVE BUTTON -->
-    <?php if(!$is_edit) print showContestSuscribeLink($node); ?>
-    <!-- END FOLLOW OR LEAVE BUTTON -->
-    
-    <!-- FOLLOWERS NUMBER -->
-    <?php if(!$is_edit) print showNumContestFollowers($node); ?>
-    <!-- END FOLLOWERS NUMBER -->
-    
-    <!-- COMMENTS NUMBER FROM OG_STATISTICS -->
-    <?php $num_comments = 0; 
-    if(!$is_edit && !empty($node->field_contest_related_group[0]['nid'])){
-        $stat = og_statistics_load($node->field_contest_related_group[0]['nid']);
-        $num_comments = $stat['comments_count'];
-    }
-    ?>
-    <?php if(!$is_edit): ?>
-        <div class="contest-comments">
-            <span>
-                <?php print $num_comments; ?>
-            </span>
-        </div>
-    <?php endif; ?>
-    <!-- END COMMENTS NUMBER FROM OG_STATISTICS -->
 
-    <!-- Public Votation Link -->
-    <?php if(!$is_edit): ?>  
-        <?php global $user; 
-        if($node->field_contest_state[0]['value']==ContestState::PUBLIC_CONTEST && $user->uid!=0){
-            print l('<span>'.t('Vote your favorite').'</span>','contest/'.$node->nid.'/publicvotation',array(
-                'attributes' => array(
-                    'title' => t('Vote your favorite'),
-                    'class' => 'public-votation-link',
-                ),
-                'html' => TRUE,
-            ));
-        } ?> 
-    <?php endif; ?>
-    <!-- End Public Votation Link -->
-    
     <?php if ($node_top && !$teaser): ?>
     <div id="node-top" class="node-top row nested">
       <div id="node-top-inner" class="node-top-inner inner">
@@ -161,14 +153,14 @@
       <?php print $terms; ?>
     </div>
     <?php endif;?>
-    
+
     <div class="content clearfix<?php print ($node_right && !$teaser?' node-right':''); ?>">
-        <div class="node-content-main">  
-            <?php 
+        <div class="node-content-main">
+            <?php
                 /* QUICK TABS */
                 if(!$is_edit && isset($contest_quicktab)){
                     print $contest_quicktab;
-                }   
+                }
                 /* END QUICK TABS */
                 $content = str_replace($body, '', $content);
                 print $content;
@@ -182,7 +174,7 @@
         </div><!-- /node-right -->
         <?php endif; ?>
     </div>
-    
+
     <!-- Translate link -->
     <?php if(isset($node->content['nivaria_translate_content_link']) && !empty($node->content['nivaria_translate_content_link']['#value'])): ?>
     <div class="node-translate-link">
@@ -190,7 +182,7 @@
     </div>
     <?php endif; ?>
     <!-- End Translate link -->
-    
+
     <!-- Mark Highlighted in Newsletter -->
     <?php if(user_access(PERM_ADMIN_CONTESTS)) : ?>
     <div class="link-highlight-arquideas-newsletter">
@@ -199,15 +191,15 @@
     <?php endif; ?>
     <!-- End Mark Highlighted in Newsletter -->
 
-    
+
     <?php if ($links): ?>
     <div class="links">
       <?php print $links; ?>
     </div>
     <?php endif; ?>
-    
-    
-    
+
+
+
   </div><!-- /inner -->
 
   <?php if ($node_bottom && !$teaser): ?>
